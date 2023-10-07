dbc6e6aa8db5f98b67481dba0088d08520a148aa
Martyna B <banas.martynaa@gmail.com>
2023-10-07 15:52:08 +0200

    form

--- app/formularz/page.js
+++ app/formularz/page.js
@@ -25,37 +25,6 @@ export default function Contact() {
             </div>
          </section>
 
-         <section className='pb-16 lg:pb-36'>
-            <div ref={ref2} className="container">
-               <form action="/formularz" method="post" name="contact" data-netlify="true" 
-                  className={`max-w-[61.25rem] mx-auto flex flex-wrap justify-between transition delay-200 duration-400 ${isInView2 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
-
-                  <input type="hidden" name="form-name" value="contact" />
-
-                  <label className='custom-width-half mb-6 xs:mb-0'>
-                     Imię i nazwisko*
-                     <input type="text" name="name" className="w-full border-dark border p-3 mt-2" required/>
-                  </label>
-
-                  <label className='custom-width-half'>
-                     E-mail*
-                     <input type="email" name="email" className="w-full border-dark border p-3 mt-2" required/>
-                  </label>
-
-                  <label className='w-full mt-6 xs:mt-12'>
-                     Wiadomość*
-                     <textarea name="message" required className="w-full border-dark border p-2 mt-2 min-h-[20rem] max-h-[40rem]"/>
-                  </label>
-
-                  <input type="submit" value="Wyślij" className='mt-10 mx-auto bg-orange hover:bg-orange-hover hover:shadow-lg transition py-3 px-4 text-xl font-second text-white cursor-pointer font-bold uppercase'/>
-                  
-                  
-                  
-               </form>
-            </div>
-         </section>
-
-
 
      </>
   )
